510530df274a3c12e482cdec6172478b849ffc70
kAmMa <kamma@shrt.cz>
2017-09-20 17:22:33 +0200

    fixes

--- pattern-1/artifacts/deploy-kubernetes.sh
+++ pattern-1/artifacts/deploy-kubernetes.sh
@@ -13,17 +13,17 @@ kubectl apply -f rdbms/rdbms-persistent-volume-claim.yaml
 kubectl apply -f rdbms/rdbms-service.yaml
 kubectl apply -f rdbms/rdbms-deployment.yaml
 
-sleep 5s
+sleep 30s
 # analytics
 echo 'deploying apim analytics ...'
 kubectl apply -f apim-analytics/wso2apim-analytics-service.yaml
 kubectl apply -f apim-analytics/wso2apim-analytics-1-service.yaml
 kubectl apply -f apim-analytics/wso2apim-analytics-2-service.yaml
 kubectl apply -f apim-analytics/wso2apim-analytics-1-deployment.yaml
-sleep 5s
+sleep 10s
 kubectl apply -f apim-analytics/wso2apim-analytics-2-deployment.yaml
 
-sleep 5s
+sleep 1m
 # apim
 kubectl apply -f apim/wso2apim-mgt-volume-claim.yaml
 kubectl apply -f apim/wso2apim-worker-volume-claim.yaml
@@ -32,6 +32,6 @@ kubectl apply -f apim/wso2apim-manager-worker-service.yaml
 kubectl apply -f apim/wso2apim-worker-service.yaml
 echo 'deploying apim manager-worker ...'
 kubectl apply -f apim/wso2apim-manager-worker-deployment.yaml
-sleep 5s
+sleep 1m
 echo 'deploying apim worker ...'
 kubectl apply -f apim/wso2apim-worker-deployment.yaml
