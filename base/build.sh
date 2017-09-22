09989057ec8e6e73223c330734bfebe5ebe45a76
kAmMa <kamma@shrt.cz>
2017-09-22 08:05:52 +0200

    local registry and fixed SSL handshake for KubeAPI calls

--- base/build.sh
+++ base/build.sh
@@ -9,8 +9,15 @@ analytics_dir=$(cd "${this_dir}/analytics"; pwd)
 apim_dir=$(cd "${this_dir}/apim"; pwd)
 mysql_dir=$(cd "${this_dir}/mysql"; pwd)
 
-docker build -t kammacz/rsync:1.0.0 $rsync_dir --squash
-docker build -t kammacz/sshd:1.0.0 $sshd_dir --squash
-docker build -t kammacz/wso2am:2.1.0 $apim_dir --squash
-docker build -t kammacz/wso2am-analytics:2.1.0 $analytics_dir --squash
-docker build -t kammacz/apim-rdbms-kubernetes:2.1.0 $mysql_dir --squash
+docker build -t rsync:1.0.0 $rsync_dir
+docker build -t sshd:1.0.0 $sshd_dir
+docker build -t wso2am:2.1.0 $apim_dir
+docker build -t wso2am-analytics:2.1.0 $analytics_dir
+docker build -t apim-rdbms-kubernetes:2.1.0 $mysql_dir
+
+#docker build -t rsync:1.0.0 $rsync_dir --squash
+#docker build -t sshd:1.0.0 $sshd_dir --squash
+#docker build -t wso2am:2.1.0 $apim_dir --squash
+#docker build -t wso2am-analytics:2.1.0 $analytics_dir --squash
+#docker build -t apim-rdbms-kubernetes:2.1.0 $mysql_dir --squash
+
