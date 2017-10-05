1a4171541531878d5e92ca30067c2145aa86dc1f
kAmMa <kamma@shrt.cz>
2017-10-05 10:18:20 +0200

    added mysql port mapping

--- pattern-1/artifacts/undeploy-kubernetes.sh
+++ pattern-1/artifacts/undeploy-kubernetes.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 kubectl delete serviceaccount wso2svcacct
-kubectl delete deployments,services,PersistentVolume,PersistentVolumeClaim,Ingress,configmaps -l pattern=wso2apim-pattern-1 -n wso2
-kubectl delete namespace wso2
+kubectl delete deployments,services,PersistentVolume,PersistentVolumeClaim,Ingress,configmaps -l pattern=wso2apim-pattern-1
